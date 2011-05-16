73374e6918f563687001716e90323415c1da84c0
Linus <anonymouse2048@gmail.com>
2011-05-16 21:05:22 +0200

    removed old files - these are merged into game.cpp

--- draw.h
+++
@@ -1,8 +0,0 @@
-#ifndef DRAW_H
-#define DRAW_H
-#include <gl.h>
-#include <glu.h>
-
-void draw();
-
-#endif
--- logic.h
+++
@@ -1,9 +0,0 @@
-#ifndef LOGIC_H
-#define LOGIC_H
-
-#include <SDL/SDL.h>
-#include "defines.h"
-
-void update();
-
-#endif
