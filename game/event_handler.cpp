009b3cf69a4be80950fce30158052ee80d6e77aa
Linus <anonymouse2048@gmail.com>
2011-05-20 22:52:09 +0000

    Fixed. How did I forget to include it?

--- game/event_handler.cpp
+++ game/event_handler.cpp
@@ -1,7 +1,8 @@
 #include <SDL/SDL.h>
+#include "defines.h"
 
-void handle_key(SDL_KeyboardEvent &e, communicator *State) {
+void handle_key(SDL_KeyboardEvent &e, communicator &State) {
 	if (e.keysym.sym == SDLK_ESCAPE) {
-		*State.running = false;
+		State.running = false;
 	}
 }
